63f17b85bda6666816c6fde4d2e4a059af90f230
kenshin-samourai <kenshin_samourai@tutanota.com>
2021-02-26 15:31:59 +0100

    refactor part of notifs service code into an abstract class

---
+++ accounts/notifications/abstract-notifs-service.js
@@ -0,0 +1,354 @@
+/*!
+ * accounts/notifications/abstract-notifs-service.js
+ * Copyright © 2019 – Katana Cryptographic Ltd. All Rights Reserved.
+ */
+'use strict'
+
+const _ = require('lodash')
+const LRU = require('lru-cache')
+const Logger = require('../../lib/logger')
+const WalletEntities = require('../../lib/wallet/wallet-entities')
+
+const debug = !!(process.argv.indexOf('ws-debug') > -1)
+
+
+/**
+ * An abstract class defining a notifications service
+ */
+class AbstractNotifsService {
+
+  /**
+   * Constructor
+   * @param {NotificationServer} server - notification server
+   */
+  constructor(server) {
+    // Notifications server
+    this.server = server
+    // Dictionary of connections
+    this.conn = {}
+    // Dictionary of subscriptions
+    this.subs = {}
+    // Dictionary mapping addresses to pubkeys
+    this.cachePubKeys = {}
+
+    // Cache registering the most recent subscriptions received
+    // Used to filter multiple subscriptions sent by external apps.
+    this.cacheSubs = LRU({
+      // Maximum number of subscriptions to store in cache
+      // Estimate: 1000 clients with an average of 5 subscriptions
+      max: 5000,
+      // Function used to compute length of item
+      length: (n, key) => 1,
+      // Maximum age for items in the cache (1mn)
+      maxAge: 60000
+    })
+  }
+
+  /**
+   * Register a new connection
+   * @param {object} connection - connection object
+   */
+  registerConnection(conn) {
+    conn.id = this.server.sessions++
+    conn.subs = []
+    debug && Logger.info(`API : Client ${conn.id} connected`)
+    this.conn[conn.id] = conn
+    this.server.clients = this.server.clients + 1
+    this.server.maxConn = Math.max(this.server.maxConn, Object.keys(this.conn).length)
+    return conn
+  }
+
+  /**
+   * Close a registered connection
+   * @param {object} conn - connection object
+   * @param {boolean} forcedClose - true if close initiated by server
+   */
+  _closeConnection(conn, forcedClose) {
+    try {
+      for (let topic of conn.subs) {
+        this._unsub(topic, conn.id)
+        // Close initiated by client, remove subscriptions from cache
+        if (!forcedClose && this.cacheSubs.has(topic))
+          this.cacheSubs.del(topic)
+      }
+      if (this.conn[conn.id]) {
+        delete this.conn[conn.id]
+        this.server.clients = this.server.clients - 1
+      }
+      debug && Logger.info(`API : Client ${conn.id} disconnected`)
+    } catch(e) {
+      Logger.error(e, 'API :')
+    }
+  }
+
+  /**
+   * Filter potential duplicate subscriptions
+   * @param {string} msg - subscription received
+   * @returns {boolean} returns false if it's a duplicate, true otherwise.
+   */
+  _filterSubscriptions(msg) {
+    if (this.cacheSubs.has(msg)) {
+      debug && Logger.info('API : Duplicate subscriptions detected')
+      return false
+    } else {
+      this.cacheSubs.set(msg, true)
+      return true
+    }
+  }
+
+  /**
+   * Subscribe to a list of addresses/xpubs/pubkeys
+   * @param {WalletEntities} wallet - entities composing the wallet
+   * @param {object} conn - connection asking for subscription
+   */
+  _walletsub(wallet, conn) {
+    for (let a in wallet.addrs) {
+      const address = wallet.addrs[a]
+      this._addsub(address, conn)
+      if (wallet.pubkeys[a])
+        this.cachePubKeys[address] = wallet.pubkeys[a]
+    }
+
+    for (let xpub of wallet.xpubs)
+      this._addsub(xpub, conn)
+  }
+
+  /**
+   * Subscribe to a topic
+   * @param {string} topic - topic
+   * @param {object} conn - connection asking for subscription
+   */
+  _addsub(topic, conn) {
+    if (conn.subs.indexOf(topic) >= 0)
+      return false
+
+    conn.subs.push(topic)
+
+    if (!this.subs[topic])
+      this.subs[topic] = []
+
+    this.subs[topic].push(conn.id)
+
+    debug && Logger.info(`API : Client ${conn.id} subscribed to ${topic}`)
+  }
+
+  /**
+   * Unsubscribe from a topic
+   * @param {string} topic - topic
+   * @param {int} cid - client id
+   */
+  _unsub(topic, cid) {
+    if (!this.subs[topic])
+      return false
+
+    const index = this.subs[topic].indexOf(cid)
+    if (index < 0)
+      return false
+
+    this.subs[topic].splice(index, 1)
+
+    if (this.subs[topic].length == 0) {
+      delete this.subs[topic]
+      if (this.cachePubKeys.hasOwnProperty(topic))
+        delete this.cachePubKeys[topic]
+    }
+
+    return true
+  }
+
+  /**
+   * Dispatch notifications for a new block
+   * @param {string} header - block header
+   */
+  notifyBlock(header) {
+    try {
+      if (!this.subs['block'])
+        return
+
+      const data = {op: 'block', x: header}
+
+      for (let cid of this.subs['block']) {
+        if (!this.conn[cid])
+          continue
+
+        try {
+          this._send(cid, JSON.stringify(data))
+        } catch(e) {
+          Logger.error(e, `API : Error sending notification for 'block' to client ${cid}`)
+        }
+      }
+    } catch(e) {
+      Logger.error(e, `API :`)
+    }
+  }
+
+  /**
+   * Dispatch notifications for a transaction
+   *
+   * Transaction notification operates within these constraints:
+   *   1. Notify each client ONCE of a relevant transaction
+   *   2. Maintain privacy of other parties when transactions are between clients
+   *
+   *   Clients subscribe to a list of xpubs and addresses. Transactions identify
+   *   address and xpub if available on inputs and outputs, omitting inputs and
+   *   outputs for untracked addresses.
+   *
+   *   Example:
+   *   tx
+   *     inputs
+   *       addr1
+   *       xpub2
+   *     outputs
+   *       xpub1
+   *       xpub2
+   *       addr2
+   *       xpub3
+   *
+   *   subs
+   *     addr1: client1, client2
+   *     addr2: client1
+   *     xpub1: client1
+   *     xpub2: client2
+   *    xpub4: client3
+   *
+   *   client1: addr1, addr2, xpub1
+   *   client2: addr1, xpub2
+   *   client3: xpub4
+   *
+   *   tx -> client1
+   *     inputs
+   *       addr1
+   *     outputs
+   *       xpub1
+   *       addr2
+   *
+   *   tx -> client2
+   *     inputs
+   *       addr1
+   *       xpub2
+   *     outputs
+   *       xpub2
+   *
+   * @param {object} tx - transaction
+   *
+   * @note Synchronous processing done by this method
+   * may become a bottleneck in the future if under heavy load.
+   * Split in multiple async calls might make sense.
+   */
+  notifyTransaction(tx) {
+    try {
+      // Topics extracted from the transaction
+      const topics = {}
+      // Client subscriptions: {[cid]: [topic1, topic2, ...]}
+      const clients = {}
+
+      // Extract topics from the inputs
+      for (let i in tx.inputs) {
+        let input = tx.inputs[i]
+        let topic = null
+
+        if (input.prev_out) {
+          // Topic is either xpub or addr. Should it be both?
+          if (input.prev_out.xpub) {
+            topic = input.prev_out.xpub.m
+          } else if (input.prev_out.addr) {
+            topic = input.prev_out.addr
+          }
+        }
+
+        if (this.subs[topic]) {
+          topics[topic] = true
+          // Add topic information to the input
+          input.topic = topic
+        }
+      }
+
+      // Extract topics from the outputs
+      for (let o in tx.out) {
+        let output = tx.out[o]
+        let topic = null
+
+        if (output.xpub) {
+          topic = output.xpub.m
+        } else if (output.addr) {
+          topic = output.addr
+        }
+
+        if (this.subs[topic]) {
+          topics[topic] = true
+          // Add topic information to the output
+          output.topic = topic
+        }
+      }
+
+      for (let topic in topics) {
+        for (let cid of this.subs[topic]) {
+          if (!clients[cid])
+            clients[cid] = []
+          if (clients[cid].indexOf(topic) == -1)
+            clients[cid].push(topic)
+        }
+      }
+
+      // Tailor a transaction for each client
+      for (let cid in clients) {
+        const ctx = _.cloneDeep(tx)
+        ctx.inputs = []
+        ctx.out = []
+
+        // List of topics relevant to this client
+        const clientTopics = clients[cid]
+
+        // Check for topic information on inputs & outputs (added above)
+        for (let input of tx.inputs) {
+          const topic = input.topic
+          if (topic && clientTopics.indexOf(topic) > -1) {
+            const cin = _.cloneDeep(input)
+            delete cin.topic
+            if (this.cachePubKeys.hasOwnProperty(topic))
+              cin.pubkey = this.cachePubKeys[topic]
+            ctx.inputs.push(cin)
+          }
+        }
+
+        for (let output of tx.out) {
+          const topic = output.topic
+          if (topic && clientTopics.indexOf(topic) > -1) {
+            const cout = _.cloneDeep(output)
+            delete cout.topic
+            if (this.cachePubKeys.hasOwnProperty(topic))
+              cout.pubkey = this.cachePubKeys[topic]
+            ctx.out.push(cout)
+          }
+        }
+
+        // Move on if the custom transaction has no inputs or outputs
+        if (ctx.inputs.length == 0 && ctx.out.length == 0)
+          continue
+
+        // Send custom transaction to client
+        const data = {op: 'utx', x: ctx}
+
+        try {
+          this._send(cid, JSON.stringify(data))
+          debug && Logger.error(`API : Sent ctx ${ctx.hash} to client ${cid}`)
+        } catch(e) {
+          Logger.error(e, `API : Trouble sending ctx to client ${cid}`)
+        }
+      }
+
+    } catch(e) {
+      Logger.error(e, `API :`)
+    }
+  }
+
+  /**
+   * Send a notification message
+   * @param {int} cid - client id
+   * @param {string} msg - message
+   */
+  _send(cid, msg) {}
+
+}
+
+module.exports = AbstractNotifsService
--- accounts/notifications/websockets-service.js
+++ accounts/notifications/websockets-service.js
@@ -5,13 +5,11 @@
 'use strict'
 
 const _ = require('lodash')
-const LRU = require('lru-cache')
 const WebSocket = require('websocket')
 const Logger = require('../../lib/logger')
-const network = require('../../lib/bitcoin/network')
-const keys = require('../../keys')[network.key]
 const apiHelper = require('../api-helper')
 const authMgr = require('../../lib/auth/authorizations-manager')
+const AbstractNotifsService = require('./abstract-notifs-service')
 
 const debug = !!(process.argv.indexOf('ws-debug') > -1)
 
@@ -19,36 +17,16 @@ const debug = !!(process.argv.indexOf('ws-debug') > -1)
 /**
  * A class providing a notifications service over web sockets
  */
-class WebsocketsNotifsService {
+class WebsocketsNotifsService extends AbstractNotifsService {
 
   /**
    * Constructor
    * @param {NotificationServer} server - notification server
    */
   constructor(server) {
-    // Notifications server
-    this.server = server
+    super(server)
     // Web sockets server
     this.ws = null
-    // Dictionary of connections
-    this.conn = {}
-    // Dictionary of subscriptions
-    this.subs = {}
-    // Dictionary mapping addresses to pubkeys
-    this.cachePubKeys = {}
-
-    // Cache registering the most recent subscriptions received
-    // Used to filter multiple subscriptions sent by external apps.
-    this.cacheSubs = LRU({
-      // Maximum number of subscriptions to store in cache
-      // Estimate: 1000 clients with an average of 5 subscriptions
-      max: 5000,
-      // Function used to compute length of item
-      length: (n, key) => 1,
-      // Maximum age for items in the cache (1mn)
-      maxAge: 60000
-    })
-
     // Initialize the web socket server
     this._initWSServer()
   }
@@ -63,11 +41,7 @@ class WebsocketsNotifsService {
 
     this.ws.on('request', req => {
       try {
-        let conn = req.accept(null, req.origin)
-        conn.id = this.server.sessions++
-        conn.subs = []
-
-        debug && Logger.info(`API : Client ${conn.id} connected`)
+        let conn = this.registerConnection(req.accept(null, req.origin))
 
         conn.on('close', () => {
           this._closeWSConnection(conn, false)
@@ -86,10 +60,6 @@ class WebsocketsNotifsService {
             this._closeWSConnection(conn, true)
         })
 
-        this.conn[conn.id] = conn
-        this.server.clients = this.server.clients + 1
-        this.server.maxConn = Math.max(this.server.maxConn, Object.keys(this.conn).length)
-
       } catch(e) {
         Logger.error(e, `API : WebsocketsNotifsService._initWSServer() : Error during request accept`)
       }
@@ -103,42 +73,12 @@ class WebsocketsNotifsService {
    */
   _closeWSConnection(conn, forcedClose) {
     try {
-      for (let topic of conn.subs) {
-        this._unsub(topic, conn.id)
-
-        // Close initiated by client, remove subscriptions from cache
-        if (!forcedClose && this.cacheSubs.has(topic))
-          this.cacheSubs.del(topic)
-      }
-
-      if (this.conn[conn.id]) {
-        delete this.conn[conn.id]
-        this.server.clients = this.server.clients - 1
-      }
-
+      this._closeConnection(conn, forcedClose)
       // Close initiated by server, drop the connection
       if (forcedClose && conn.connected)
         conn.drop(1008, 'Get out of here!')
-
-      debug && Logger.info(`API : Client ${conn.id} disconnected`)
-
     } catch(e) {
       Logger.error(e, 'API : WebsocketsNotifsService._closeWSConnection()')
-    }
-  }
-
-  /**
-   * Filter potential duplicate subscriptions
-   * @param {string} msg - subscription received
-   * @returns {boolean} returns false if it's a duplicate, true otherwise.
-   */
-  _filterWSMessage(msg) {
-    if (this.cacheSubs.has(msg)) {
-      debug && Logger.info('API : Duplicate subscriptions detected')
-      return false
-    } else {
-      this.cacheSubs.set(msg, true)
-      return true
     }
   }
 
@@ -172,10 +112,12 @@ class WebsocketsNotifsService {
           if (data.addr) {
             // Check for potential flood by clients
             // subscribing for the same xpub again and again
-            if (this._filterWSMessage(data.addr))
-              this._entitysub(data.addr, conn)
-            else
+            if (this._filterSubscriptions(data.addr)) {
+              const walletEntities = apiHelper.parseEntities(data.addr)
+              this._walletsub(walletEntities, conn)
+            } else {
               this._closeWSConnection(conn, true)
+            }
           }
           break
         case 'blocks_sub':
@@ -188,267 +130,12 @@ class WebsocketsNotifsService {
   }
 
   /**
-   * Subscribe to a list of addresses/xpubs/pubkeys
-   * @param {string} topic - topic
-   * @param {object} conn - connection asking for subscription
-   */
-  _entitysub(topic, conn) {
-    const valid = apiHelper.parseEntities(topic)
-
-    for (let a in valid.addrs) {
-      const address = valid.addrs[a]
-      this._addsub(address, conn)
-      if (valid.pubkeys[a]) {
-        this.cachePubKeys[address] = valid.pubkeys[a]
-      }
-    }
-
-    for (let xpub of valid.xpubs)
-      this._addsub(xpub, conn)
-  }
-
-  /**
-   * Subscribe to a topic
-   * @param {string} topic - topic
-   * @param {object} conn - connection asking for subscription
-   */
-  _addsub(topic, conn) {
-    if (conn.subs.indexOf(topic) >= 0)
-      return false
-
-    conn.subs.push(topic)
-
-    if (!this.subs[topic])
-      this.subs[topic] = []
-
-    this.subs[topic].push(conn.id)
-
-    debug && Logger.info(`API : Client ${conn.id} subscribed to ${topic}`)
-  }
-
-  /**
-   * Unsubscribe from a topic
-   * @param {string} topic - topic
+   * Send a notification message
    * @param {int} cid - client id
+   * @param {string} msg - message
    */
-  _unsub(topic, cid) {
-    if (!this.subs[topic])
-      return false
-
-    const index = this.subs[topic].indexOf(cid)
-    if (index < 0)
-      return false
-
-    this.subs[topic].splice(index, 1)
-
-    if (this.subs[topic].length == 0) {
-      delete this.subs[topic]
-      if (this.cachePubKeys.hasOwnProperty(topic))
-        delete this.cachePubKeys[topic]
-    }
-
-    return true
-  }
-
-  /**
-   * Dispatch a notification to all clients
-   * who have subscribed to a topic
-   * @param {string} topic - topic
-   * @param {string} msg - content of the notification
-   */
-  dispatch(topic, msg) {
-    if (!this.subs[topic])
-      return
-
-    for (let cid of this.subs[topic]) {
-      if (!this.conn[cid])
-        continue
-
-      try {
-        this.conn[cid].sendUTF(msg)
-      } catch(e) {
-        Logger.error(e, `API : WebsocketsNotifsService.dispatch() : Error sending dispatch for ${topic} to client ${cid}`)
-      }
-    }
-  }
-
-  /**
-   * Dispatch notifications for a new block
-   * @param {string} header - block header
-   */
-  notifyBlock(header) {
-    try {
-      const data = {
-        op: 'block',
-        x: header
-      }
-      this.dispatch('block', JSON.stringify(data))
-    } catch(e) {
-      Logger.error(e, `API : WebsocketsNotifsService.notifyBlock()`)
-    }
-  }
-
-  /**
-   * Dispatch notifications for a transaction
-   *
-   * Transaction notification operates within these constraints:
-   *   1. Notify each client ONCE of a relevant transaction
-   *   2. Maintain privacy of other parties when transactions are between clients
-   *
-   *   Clients subscribe to a list of xpubs and addresses. Transactions identify
-   *   address and xpub if available on inputs and outputs, omitting inputs and
-   *   outputs for untracked addresses.
-   *
-   *   Example:
-   *   tx
-   *     inputs
-   *       addr1
-   *       xpub2
-   *     outputs
-   *       xpub1
-   *       xpub2
-   *       addr2
-   *       xpub3
-   *
-   *   subs
-   *     addr1: client1, client2
-   *     addr2: client1
-   *     xpub1: client1
-   *     xpub2: client2
-   *    xpub4: client3
-   *
-   *   client1: addr1, addr2, xpub1
-   *   client2: addr1, xpub2
-   *   client3: xpub4
-   *
-   *   tx -> client1
-   *     inputs
-   *       addr1
-   *     outputs
-   *       xpub1
-   *       addr2
-   *
-   *   tx -> client2
-   *     inputs
-   *       addr1
-   *       xpub2
-   *     outputs
-   *       xpub2
-   *
-   * @param {object} tx - transaction
-   *
-   * @note Synchronous processing done by this method
-   * may become a bottleneck in the future if under heavy load.
-   * Split in multiple async calls might make sense.
-   */
-  notifyTransaction(tx) {
-    try {
-      // Topics extracted from the transaction
-      const topics = {}
-      // Client subscriptions: {[cid]: [topic1, topic2, ...]}
-      const clients = {}
-
-      // Extract topics from the inputs
-      for (let i in tx.inputs) {
-        let input = tx.inputs[i]
-        let topic = null
-
-        if (input.prev_out) {
-          // Topic is either xpub or addr. Should it be both?
-          if (input.prev_out.xpub) {
-            topic = input.prev_out.xpub.m
-          } else if (input.prev_out.addr) {
-            topic = input.prev_out.addr
-          }
-        }
-
-        if (this.subs[topic]) {
-          topics[topic] = true
-          // Add topic information to the input
-          input.topic = topic
-        }
-      }
-
-      // Extract topics from the outputs
-      for (let o in tx.out) {
-        let output = tx.out[o]
-        let topic = null
-
-        if (output.xpub) {
-          topic = output.xpub.m
-        } else if (output.addr) {
-          topic = output.addr
-        }
-
-        if (this.subs[topic]) {
-          topics[topic] = true
-          // Add topic information to the output
-          output.topic = topic
-        }
-      }
-
-      for (let topic in topics) {
-        for (let cid of this.subs[topic]) {
-          if (!clients[cid])
-            clients[cid] = []
-          if (clients[cid].indexOf(topic) == -1)
-            clients[cid].push(topic)
-        }
-      }
-
-      // Tailor a transaction for each client
-      for (let cid in clients) {
-        const ctx = _.cloneDeep(tx)
-        ctx.inputs = []
-        ctx.out = []
-
-        // List of topics relevant to this client
-        const clientTopics = clients[cid]
-
-        // Check for topic information on inputs & outputs (added above)
-        for (let input of tx.inputs) {
-          const topic = input.topic
-          if (topic && clientTopics.indexOf(topic) > -1) {
-            const cin = _.cloneDeep(input)
-            delete cin.topic
-            if (this.cachePubKeys.hasOwnProperty(topic))
-              cin.pubkey = this.cachePubKeys[topic]
-            ctx.inputs.push(cin)
-          }
-        }
-
-        for (let output of tx.out) {
-          const topic = output.topic
-          if (topic && clientTopics.indexOf(topic) > -1) {
-            const cout = _.cloneDeep(output)
-            delete cout.topic
-            if (this.cachePubKeys.hasOwnProperty(topic))
-              cout.pubkey = this.cachePubKeys[topic]
-            ctx.out.push(cout)
-          }
-        }
-
-        // Move on if the custom transaction has no inputs or outputs
-        if (ctx.inputs.length == 0 && ctx.out.length == 0)
-          continue
-
-        // Send custom transaction to client
-        const data = {
-          op: 'utx',
-          x: ctx
-        }
-
-        try {
-          this.conn[cid].sendUTF(JSON.stringify(data))
-          debug && Logger.error(`API : Sent ctx ${ctx.hash} to client ${cid}`)
-        } catch(e) {
-          Logger.error(e, `API : .notifyTransaction() : Trouble sending ctx to client ${cid}`)
-        }
-      }
-
-    } catch(e) {
-      Logger.error(e, `API : WebsocketsNotifsService.notifyTransaction()`)
-    }
+  _send(cid, msg) {
+    this.conn[cid].sendUTF(msg)
   }
 
   /**
@@ -457,19 +144,15 @@ class WebsocketsNotifsService {
    * @param {integer} cid - connection id
    */
   notifyAuthError(err, cid) {
-    const data = {
-      op: 'error',
-      msg: err
-    }
+    const data = {op: 'error', msg: err}
 
     try {
-      this.conn[cid].sendUTF(JSON.stringify(data))
+      this._send(cid, JSON.stringify(data))
       debug && Logger.error(`API : Sent authentication error to client ${cid}`)
     } catch(e) {
       Logger.error(e, `API : WebsocketsNotifsService.notifyAuthError() : Trouble sending authentication error to client ${cid}`)
     }
   }
-
 
 }
 
